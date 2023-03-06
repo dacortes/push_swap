febf886dd076f638dc8b7a54085816d1db64d2ec
Dannyel <dacortes@car15s3.42barcelona.com>
2023-03-06 21:24:07 +0100

    libraries are added

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,11 +6,14 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:04:08 by dacortes          #+#    #+#             */
-/*   Updated: 2023/03/06 21:06:14 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/03/06 21:23:11 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include"../libft/libft.h"
+# include"../libft/ft_printf.h"
 
 #endif
