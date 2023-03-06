a3514a8ca5c246cb78309e92c37de728e9a26be4
Dannyel <dacortes@car15s3.42barcelona.com>
2023-03-06 21:56:53 +0100

    add macros and colors

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:04:08 by dacortes          #+#    #+#             */
-/*   Updated: 2023/03/06 21:23:11 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/03/06 21:56:37 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 
 # include"../libft/libft.h"
 # include"../libft/ft_printf.h"
+
+// ================================= MACROS ================================= //
+
+/* Outputs */
+# define SUCCES 0
+# define FALSE 0
+# define TRUE 1
+# define ERROR 0
+# define E_EXIT 1
+
+// ================================= COLORS ================================= //
+
+# define E "\033[m"        //end
+# define R "\033[1;31m"    //red
+# define G "\033[1;32m"    //green
+# define Y "\033[1;33m"    //yellow
+# define B "\033[1;34m"    //blue
 
 #endif
