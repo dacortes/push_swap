fcc01435869a0e817d65067485fd7742726f0c57
dacortes <dacortes@student.42barcelona.com>
2023-04-27 11:29:55 +0200

    update

--- src/sorted/the_rest.c
+++ src/sorted/the_rest.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:17:52 by dacortes          #+#    #+#             */
-/*   Updated: 2023/04/25 15:50:18 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/04/27 11:29:44 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,22 +57,50 @@ static void	number_of_chunks(t_aux *tmp, t_stack *a, t_stack *b)
 	}
 }
 
-static void	push_sorted(t_aux *tmp, t_stack *a, t_stack *b)
+static void	push_sorted(t_stack *a, t_stack *b)
 {
-	tmp->num = b->size;
 	while (b->size)
 	{
-		if (b->top->index == tmp->num)
-		{
+		if (b->top->index == b->size)
 			push(b, a, STACK_A);
-			tmp->num--;
-		}
 		else if (find_bot(b) >= b->size / 2)
 			reverse_rotate(b, STACK_B);
 		else
 			rotate(b, STACK_B);
 	}
 }
+
+// static void	number_of_chunks(t_aux *tmp, t_stack *a, t_stack *b)
+// {
+// 	tmp->chunks = a->size / 25;
+// 	while (a->size)
+// 	{
+// 		while (b->size != tmp->range_end && a->size != 0)
+// 		{
+
+// 		}
+// 		if (a->size + b->size == tmp->size_ini)
+// 		{
+// 			tmp->range_ini = tmp->range_end + 1;
+// 			tmp->range_end = tmp->range_end + 25;
+// 			tmp->num = 25;
+// 		}
+// 		tmp->chunks--;
+// 	}
+// }
+
+// static void	push_sorted(t_stack *a, t_stack *b)
+// {
+// 	while (b->size)
+// 	{
+// 		if (b->top->index == b->size)
+// 			push(b, a, STACK_A);
+// 		else if (find_bot(b) >= b->size / 2)
+// 			reverse_rotate(b, STACK_B);
+// 		else
+// 			rotate(b, STACK_B);
+// 	}
+// }
 
 int	the_rest(t_stack *a, t_stack *b)
 {
@@ -83,10 +111,11 @@ int	the_rest(t_stack *a, t_stack *b)
 	tmp.range_ini = 1;
 	tmp.range_end = 25;
 	tmp.size_ini = a->size;
+	(void)b;
 	if (a->size >= 6)
 	{
 		number_of_chunks(&tmp, a, b);
-		push_sorted(&tmp, a, b);
+		push_sorted(a, b);
 	}
 	return (FALSE);
 }
